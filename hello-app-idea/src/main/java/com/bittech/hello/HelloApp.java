1921025534d2d8568623ec8712b01ec2d3dfb939
CHSOQIWEN <44434467+CHSOQIWEN@users.noreply.github.com>
2019-07-22 19:20:41 +0800

    Maven初练习

---
+++ hello-app-idea/src/main/java/com/bittech/hello/HelloApp.java
@@ -0,0 +1,11 @@
+package com.bittech.hello;
+
+/**
+ * @author CHAOQIWEN
+ * @data 2019/7/22 17:08
+ */
+public class HelloApp {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
